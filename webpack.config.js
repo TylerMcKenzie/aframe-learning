0dc2569ca199a03ec4e723ebda0ca422754e1f37
tylermckenzie <tylrmckenz@gmail.com>
2018-03-10 20:36:16 -0600

    add init config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require("webpack");
+const path = require("path");
+const { resolve } = path;
+const src = resolve(__dirname, 'src');
+const build = resolve(__dirname, 'build');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: build,
+    filename: '[name].bundle.js'
+  },
+  optimization: {
+    minimize: false,
+    runtimeChunk: {
+      name: 'vendor'
+    },
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /node_modules/,
+          name: "vendor",
+          chunks: "initial",
+          minSize: 1
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        include: [src],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: /node_modules/
+    }
+  }
+}
